061d99223c691fa7fb54c29b695a0fc5f869a2ec
irononet <andrewwanet9@gmail.com>
2024-03-25 05:25:34 +0100

    internal module upgraded

--- internal/tools.go
+++ internal/tools.go
@@ -1,7 +1,9 @@
+//go:build tools
+// +build tools
+
 package internal
 
 import (
-	
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
