2f05b9e0a20186c8c041b3a425a5e4f9661febb0
irononet <andrewwanet9@gmail.com>
2024-03-29 03:15:38 +0100

    refactored the grpc package in baskets

--- baskets/internal/grpc/order_repository.go
+++ baskets/internal/grpc/order_repository.go
@@ -10,36 +10,36 @@ import (
 	"github.com/irononet/mallbots/ordering/orderingpb"
 )
 
-type OrderRepository struct{
+type OrderRepository struct {
 	client orderingpb.OrderingServiceClient
 }
 
 var _ domain.OrderRepository = (*OrderRepository)(nil)
 
-func NewOrderRepository(conn *grpc.ClientConn) OrderRepository{
+func NewOrderRepository(conn *grpc.ClientConn) OrderRepository {
 	return OrderRepository{client: orderingpb.NewOrderingServiceClient(conn)}
 }
 
-func (r OrderRepository) Save(ctx context.Context, basket *domain.Basket) (string, error){
-	items := make([]*orderingpb.Item, 0, len(basket.Items))
-	for _, item := range basket.Items{
+func (r OrderRepository) Save(ctx context.Context, paymentID, customerID string, basketItems map[string]domain.Item) (string, error) {
+	items := make([]*orderingpb.Item, 0, len(basketItems))
+	for _, item := range basketItems {
 		items = append(items, &orderingpb.Item{
-			StoreId: item.StoreID,
-			ProductId: item.ProductID,
-			StoreName: item.ProductName,
+			StoreId:     item.StoreID,
+			ProductId:   item.ProductID,
+			StoreName:   item.ProductName,
 			ProductName: item.ProductName,
-			Price: item.ProductPrice,
-			Quantity: int32(item.Quantity),
+			Price:       item.ProductPrice,
+			Quantity:    int32(item.Quantity),
 		})
 	}
 
 	resp, err := r.client.CreateOrder(ctx, &orderingpb.CreateOrderRequest{
-		Items: items,
-		CustomerId: basket.CustomerID,
-		PaymentId: basket.PaymentID,
+		Items:      items,
+		CustomerId: customerID,
+		PaymentId:  paymentID,
 	})
 
-	if err != nil{
+	if err != nil {
 		return "", errors.Wrap(err, "saving order")
 	}
 
