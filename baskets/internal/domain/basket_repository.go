54984288038b849b084809fb2f29c11ce970f03e
irononet <andrewwanet9@gmail.com>
2024-03-29 03:10:52 +0100

    upgraded the domain package in baskets

--- baskets/internal/domain/basket_repository.go
+++ baskets/internal/domain/basket_repository.go
@@ -1,10 +1,11 @@
 package domain
 
-import(
+import (
 	"context"
 )
 
-type BasketRepository interface{
+type BasketRepository interface {
+	Load(ctx context.Context, basketID string) (*Basket, error)
 	Find(ctx context.Context, basketId string) (*Basket, error)
 	Save(ctx context.Context, basket *Basket) error
 	Update(ctx context.Context, basket *Basket) error
