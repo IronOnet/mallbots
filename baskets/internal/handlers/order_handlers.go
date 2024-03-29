de068d9a082ee522d12d049d64805b7c90b51cd4
irononet <andrewwanet9@gmail.com>
2024-03-29 03:18:34 +0100

    refactored the handlers in baskets module

--- baskets/internal/handlers/order_handlers.go
+++ baskets/internal/handlers/order_handlers.go
@@ -1,11 +1,10 @@
 package handlers
 
 import (
-	"github.com/irononet/mallbots/baskets/internal/application"
 	"github.com/irononet/mallbots/baskets/internal/domain"
 	"github.com/irononet/mallbots/internal/ddd"
 )
 
-func RegisterOrderHandlers(orderHandlers application.DomainEventHandlers, domainSubscriber ddd.EventSubscriber) {
-	domainSubscriber.Subscribe(domain.BasketCheckedOut{}, orderHandlers.OnBasketCheckedOut)
+func RegisterOrderHandlers(orderHandlers ddd.EventHandler[ddd.AggregateEvent], domainSubscriber ddd.EventSubscriber[ddd.AggregateEvent]) {
+	domainSubscriber.Subscribe(domain.BasketCheckedOutEvent, orderHandlers)
 }
