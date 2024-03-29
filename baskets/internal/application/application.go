6540aef330b577095a4965ab3be81d1e5c175810
irononet <andrewwanet9@gmail.com>
2024-03-29 03:10:24 +0100

    upgraded application handlers in baskets module

--- baskets/internal/application/application.go
+++ baskets/internal/application/application.go
@@ -1,46 +1,45 @@
 package application
 
-import(
+import (
 	"context"
 
 	"github.com/stackus/errors"
 
 	"github.com/irononet/mallbots/baskets/internal/domain"
-	"github.com/irononet/mallbots/internal/ddd"
 )
 
 type (
-	StartBasket struct{
-		ID string
+	StartBasket struct {
+		ID         string
 		CustomerID string
 	}
 
-	CancelBasket struct{
+	CancelBasket struct {
 		ID string
 	}
 
-	CheckoutBasket struct{
-		ID string
+	CheckoutBasket struct {
+		ID        string
 		PaymentID string
 	}
 
-	AddItem struct{
-		ID string
+	AddItem struct {
+		ID        string
 		ProductID string
-		Quantity int
+		Quantity  int
 	}
 
-	RemoveItem struct{
-		ID string
+	RemoveItem struct {
+		ID        string
 		ProductID string
-		Quantity int
+		Quantity  int
 	}
 
-	GetBasket struct{
+	GetBasket struct {
 		ID string
 	}
 
-	App interface{
+	App interface {
 		StartBasket(ctx context.Context, start StartBasket) error
 		CancelBasket(ctx context.Context, cancel CancelBasket) error
 		CheckoutBasket(ctx context.Context, checkout CheckoutBasket) error
@@ -49,113 +48,109 @@ type (
 		GetBasket(ctx context.Context, get GetBasket) (*domain.Basket, error)
 	}
 
-	Application struct{
-		baskets domain.BasketRepository
-		stores domain.StoreRepository
+	Application struct {
+		baskets  domain.BasketRepository
+		stores   domain.StoreRepository
 		products domain.ProductRepository
-		orders domain.OrderRepository
-		domainPublisher ddd.EventPublisher
+		orders   domain.OrderRepository
 	}
 )
 
 var _ App = (*Application)(nil)
 
-func New(baskets domain.BasketRepository, stores domain.StoreRepository, products domain.ProductRepository, orders domain.OrderRepository, domainPublisher ddd.EventPublisher) *Application{
+func New(baskets domain.BasketRepository, stores domain.StoreRepository, products domain.ProductRepository, orders domain.OrderRepository) *Application {
 	return &Application{
-		baskets: baskets,
-		stores: stores,
+		baskets:  baskets,
+		stores:   stores,
 		products: products,
-		orders: orders,
-		domainPublisher: domainPublisher,
+		orders:   orders,
 	}
 }
 
-func (a Application) StartBasket(ctx context.Context, start StartBasket) error{
+func (a Application) StartBasket(ctx context.Context, start StartBasket) error {
 	basket, err := domain.StartBasket(start.ID, start.CustomerID)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
 	return a.baskets.Save(ctx, basket)
 }
 
-func (a Application) CancelBasket(ctx context.Context, cancel CancelBasket) error{
+func (a Application) CancelBasket(ctx context.Context, cancel CancelBasket) error {
 	basket, err := a.baskets.Find(ctx, cancel.ID)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
 	err = basket.Cancel()
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
 	return a.baskets.Update(ctx, basket)
 }
 
-func (a Application) CheckoutBasket(ctx context.Context, checkout CheckoutBasket) error{
-	basket, err := a.baskets.Find(ctx, checkout.ID)
-	if err != nil{
+func (a Application) CheckoutBasket(ctx context.Context, checkout CheckoutBasket) error {
+	basket, err := a.baskets.Load(ctx, checkout.ID)
+	if err != nil {
 		return err
 	}
 
 	err = basket.Checkout(checkout.PaymentID)
-	if err != nil{
-		return errors.Wrap(err, "baskets checkout")
+	if err != nil {
+		return errors.Wrap(err, "basket checkout")
 	}
 
-	// Submit the basket to the order module
-	_, err = a.orders.Save(ctx, basket)
-	if err != nil{
-		return errors.Wrap(err, "baskets checkout")
+	if err = a.baskets.Save(ctx, basket); err != nil {
+		return errors.Wrap(err, "basket checkout")
 	}
 
-	return errors.Wrap(a.baskets.Update(ctx, basket), "basket checkout")
+	return nil
 }
 
-func (a Application) AddItem(ctx context.Context, add AddItem) error{
+func (a Application) AddItem(ctx context.Context, add AddItem) error {
 	basket, err := a.baskets.Find(ctx, add.ID)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
 	product, err := a.products.Find(ctx, add.ProductID)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
 	store, err := a.stores.Find(ctx, product.StoreID)
-	if err != nil{
+	if err != nil {
 		return nil
 	}
 
 	err = basket.AddItem(store, product, add.Quantity)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
 	return a.baskets.Update(ctx, basket)
 }
 
-func (a Application) RemoveItem(ctx context.Context, remove RemoveItem) error{
+func (a Application) RemoveItem(ctx context.Context, remove RemoveItem) error {
 	product, err := a.products.Find(ctx, remove.ProductID)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
 	basket, err := a.baskets.Find(ctx, remove.ID)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
 	err = basket.RemoveItem(product, remove.Quantity)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
 	return a.baskets.Update(ctx, basket)
 }
 
-func (a Application) GetBasket(ctx context.Context, get GetBasket) (*domain.Basket, error){
+func (a Application) GetBasket(ctx context.Context, get GetBasket) (*domain.Basket, error) {
 	return a.baskets.Find(ctx, get.ID)
 }
--- baskets/internal/application/order_handlers.go
+++ baskets/internal/application/order_handlers.go
@@ -7,19 +7,28 @@ import (
 	"github.com/irononet/mallbots/internal/ddd"
 )
 
-type OrderHandlers struct {
+type OrderHandlers[T ddd.AggregateEvent] struct {
 	orders domain.OrderRepository
-	ignoreUnimplementedDomainEvents
 }
 
-func NewOrderHandlers(orders domain.OrderRepository) OrderHandlers {
-	return OrderHandlers{
+var _ ddd.EventHandler[ddd.AggregateEvent] = (*OrderHandlers[ddd.AggregateEvent])(nil)
+
+func NewOrderHandlers(orders domain.OrderRepository) OrderHandlers[ddd.AggregateEvent] {
+	return OrderHandlers[ddd.AggregateEvent]{
 		orders: orders,
 	}
 }
 
-func (h OrderHandlers) OnBasketCheckedOut(ctx context.Context, event ddd.Event) error {
-	checkedOut := event.(domain.BasketCheckedOut)
-	_, err := h.orders.Save(ctx, checkedOut.Basket)
+func (h OrderHandlers[T]) HandleEvent(ctx context.Context, event T) error {
+	switch event.EventName() {
+	case domain.BasketCheckedOutEvent:
+		return h.OnBasketCheckedOut(ctx, event)
+	}
+	return nil
+}
+
+func (h OrderHandlers[T]) OnBasketCheckedOut(ctx context.Context, event ddd.AggregateEvent) error {
+	checkedOut := event.Payload().(*domain.BasketCheckedOut)
+	_, err := h.orders.Save(ctx, checkedOut.PaymentID, checkedOut.CustomerID, checkedOut.Items)
 	return err
 }
