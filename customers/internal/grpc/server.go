38b249200f2847dbadcdc73d7ccf60967ad2fdf8
irononet <andrewwanet9@gmail.com>
2024-03-29 01:56:22 +0100

    updated the customerFromDomain method customer id field

--- customers/internal/grpc/server.go
+++ customers/internal/grpc/server.go
@@ -11,29 +11,29 @@ import (
 	"github.com/irononet/mallbots/customers/internal/domain"
 )
 
-type server struct{
+type server struct {
 	app application.App
 	customerspb.UnimplementedCustomersServiceServer
 }
 
 var _ customerspb.CustomersServiceServer = (*server)(nil)
 
-func RegisterServer(app application.App, registrar grpc.ServiceRegistrar) error{
+func RegisterServer(app application.App, registrar grpc.ServiceRegistrar) error {
 	customerspb.RegisterCustomersServiceServer(registrar, server{app: app})
 	return nil
 }
 
-func (s server) RegisterCustomer(ctx context.Context, request *customerspb.RegisterCustomerRequest) (*customerspb.RegisterCustomerResponse, error){
+func (s server) RegisterCustomer(ctx context.Context, request *customerspb.RegisterCustomerRequest) (*customerspb.RegisterCustomerResponse, error) {
 	id := uuid.New().String()
 	err := s.app.RegisterCustomer(ctx, application.RegisterCustomer{
-		ID: id,
-		Name: request.GetName(),
+		ID:        id,
+		Name:      request.GetName(),
 		SmsNumber: request.GetSmsNumber(),
 	})
 	return &customerspb.RegisterCustomerResponse{Id: id}, err
 }
 
-func (s server) AuthorizeCustomer(ctx context.Context, request *customerspb.AuthorizeCustomerRequest) (*customerspb.AuthorizeCustomerResponse, error){
+func (s server) AuthorizeCustomer(ctx context.Context, request *customerspb.AuthorizeCustomerRequest) (*customerspb.AuthorizeCustomerResponse, error) {
 	err := s.app.AuthorizeCustomer(ctx, application.AuthorizeCustomer{
 		ID: request.GetId(),
 	})
@@ -41,11 +41,11 @@ func (s server) AuthorizeCustomer(ctx context.Context, request *customerspb.Auth
 	return &customerspb.AuthorizeCustomerResponse{}, err
 }
 
-func (s server) GetCustomer(ctx context.Context, request *customerspb.GetCustomerRequest) (*customerspb.GetCustomerResponse, error){
+func (s server) GetCustomer(ctx context.Context, request *customerspb.GetCustomerRequest) (*customerspb.GetCustomerResponse, error) {
 	customer, err := s.app.GetCustomer(ctx, application.GetCustomer{
 		ID: request.GetId(),
 	})
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 
@@ -54,21 +54,21 @@ func (s server) GetCustomer(ctx context.Context, request *customerspb.GetCustome
 	}, nil
 }
 
-func (s server) EnableCustomer(ctx context.Context, request *customerspb.EnableCustomerRequest) (*customerspb.EnableCustomerResponse, error){
+func (s server) EnableCustomer(ctx context.Context, request *customerspb.EnableCustomerRequest) (*customerspb.EnableCustomerResponse, error) {
 	err := s.app.EnableCustomer(ctx, application.EnableCustomer{ID: request.GetId()})
 	return &customerspb.EnableCustomerResponse{}, err
 }
 
-func (s server) DisableCustomer(ctx context.Context, request *customerspb.DisableCustomerRequest) (*customerspb.DisableCustomerResponse, error){
+func (s server) DisableCustomer(ctx context.Context, request *customerspb.DisableCustomerRequest) (*customerspb.DisableCustomerResponse, error) {
 	err := s.app.DisableCustomer(ctx, application.DisableCustomer{ID: request.GetId()})
 	return &customerspb.DisableCustomerResponse{}, err
 }
 
-func (s server) customerFromDomain(customer *domain.Customer) *customerspb.Customer{
+func (s server) customerFromDomain(customer *domain.Customer) *customerspb.Customer {
 	return &customerspb.Customer{
-		Id: customer.ID,
-		Name: customer.Name,
+		Id:        customer.ID(),
+		Name:      customer.Name,
 		SmsNumber: customer.SmsNumber,
-		Enabled: customer.Enabled,
+		Enabled:   customer.Enabled,
 	}
 }
