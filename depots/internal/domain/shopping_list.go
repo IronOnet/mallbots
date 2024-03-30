d42f7baac9b6b4d73f1aeb4ce9b12edd00d5e51f
irononet <andrewwanet9@gmail.com>
2024-03-30 04:37:17 +0100

    refactored the depots/domain package

--- depots/internal/domain/shopping_list.go
+++ depots/internal/domain/shopping_list.go
@@ -2,55 +2,59 @@ package domain
 
 import (
 	"github.com/stackus/errors"
-	
+
 	"github.com/irononet/mallbots/internal/ddd"
 )
 
+const ShoppingListAggregate = "depot.ShoppingList"
+
 var (
-	ErrShoppingCannotBeCanceled = errors.Wrap(errors.ErrBadRequest, "the shopping list cannot be canceled")
-	ErrShoppingCannotBeAssigned = errors.Wrap(errors.ErrBadRequest, "the shopping list cannot be assigned")
+	ErrShoppingCannotBeCanceled  = errors.Wrap(errors.ErrBadRequest, "the shopping list cannot be canceled")
+	ErrShoppingCannotBeAssigned  = errors.Wrap(errors.ErrBadRequest, "the shopping list cannot be assigned")
 	ErrShoppingCannotBeCompleted = errors.Wrap(errors.ErrBadRequest, "the shopping list cannot be completed")
 )
 
-type ShoppingList struct{
-	ddd.AggregateBase
-	OrderId string
-	Stops Stops
+type ShoppingList struct {
+	ddd.Aggregate
+	OrderId       string
+	Stops         Stops
 	AssignedBotId string
-	Status ShoppingListStatus
+	Status        ShoppingListStatus
 }
 
-func CreateShopping(id, orderID string) *ShoppingList{
-	shoppingList := &ShoppingList{
-		AggregateBase: ddd.AggregateBase{
-			ID: id,
-		},
-		OrderId: orderID,
-		Status: ShoppingListIsAvailable,
-		Stops: make(Stops),
+func NewShoppingList(id string) *ShoppingList {
+	return &ShoppingList{
+		Aggregate: ddd.NewAggregate(id, ShoppingListAggregate),
 	}
+}
 
-	shoppingList.AddEvent(&ShoppingListCreated{
+func CreateShopping(id, orderID string) *ShoppingList {
+	shoppingList := NewShoppingList(id)
+	shoppingList.OrderId = orderID
+	shoppingList.Status = ShoppingListIsAvailable
+	shoppingList.Stops = make(Stops)
+
+	shoppingList.AddEvent(ShoppingListCreatedEvent, &ShoppingListCreated{
 		ShoppingList: shoppingList,
 	})
 
 	return shoppingList
 }
 
-func (sl *ShoppingList) AddItem(store *Store, product *Product, quantity int) error{
-	if _, exists := sl.Stops[store.ID]; !exists{
+func (sl *ShoppingList) AddItem(store *Store, product *Product, quantity int) error {
+	if _, exists := sl.Stops[store.ID]; !exists {
 		sl.Stops[store.ID] = &Stop{
-			StoreName: store.Name,
+			StoreName:     store.Name,
 			StoreLocation: store.Location,
-			Items: make(Items),
+			Items:         make(Items),
 		}
 	}
 
 	return sl.Stops[store.ID].AddItem(product, quantity)
 }
 
-func (sl ShoppingList) isCanceleable() bool{
-	switch sl.Status{
+func (sl ShoppingList) isCanceleable() bool {
+	switch sl.Status {
 	case ShoppingListIsAvailable, ShoppingListIsAssigned, ShoppingListIsActive:
 		return true
 	default:
@@ -58,52 +62,52 @@ func (sl ShoppingList) isCanceleable() bool{
 	}
 }
 
-func (sl *ShoppingList) Cancel() error{
-	if !sl.isCanceleable(){
+func (sl *ShoppingList) Cancel() error {
+	if !sl.isCanceleable() {
 		return ErrShoppingCannotBeAssigned
 	}
 
 	sl.Status = ShoppingListIsCanceled
 
-	sl.AddEvent(&ShoppingListCanceled{
+	sl.AddEvent(ShoppingListCanceledEvent, &ShoppingListCanceled{
 		ShoppingList: sl,
 	})
 
 	return nil
 }
 
-func (sl *ShoppingList) isAssignable() bool{
+func (sl *ShoppingList) isAssignable() bool {
 	return sl.Status == ShoppingListIsAvailable
 }
 
-func (sl *ShoppingList) Assign(id string) error{
-	if !sl.isAssignable(){
+func (sl *ShoppingList) Assign(id string) error {
+	if !sl.isAssignable() {
 		return ErrShoppingCannotBeAssigned
 	}
 
 	sl.AssignedBotId = id
 	sl.Status = ShoppingListIsAssigned
 
-	sl.AddEvent(&ShoppingListAssigned{
+	sl.AddEvent(ShoppingListAssignedEvent, &ShoppingListAssigned{
 		ShoppingList: sl,
-		BotID: id,
+		BotID:        id,
 	})
 
 	return nil
 }
 
-func (sl ShoppingList) isCompletable() bool{
+func (sl ShoppingList) isCompletable() bool {
 	return sl.Status == ShoppingListIsAssigned
 }
 
-func (sl *ShoppingList) Complete() error{
-	if !sl.isCompletable(){
+func (sl *ShoppingList) Complete() error {
+	if !sl.isCompletable() {
 		return ErrShoppingCannotBeCompleted
 	}
 
 	sl.Status = ShoppingListIsCompleted
 
-	sl.AddEvent(&ShoppingListCompleted{
+	sl.AddEvent(ShoppingListCompletedEvent, &ShoppingListCompleted{
 		ShoppingList: sl,
 	})
 
--- depots/internal/domain/shopping_list_events.go
+++ depots/internal/domain/shopping_list_events.go
@@ -1,26 +1,41 @@
 package domain
 
-type ShoppingListCreated struct{
+const (
+	ShoppingListCreatedEvent   = "depots.ShoppingListCreated"
+	ShoppingListCanceledEvent  = "depots.ShoppingListCanceled"
+	ShoppingListAssignedEvent  = "depots.ShoppingListAssigned"
+	ShoppingListCompletedEvent = "depots.ShoppingListCompleted"
+)
+
+type ShoppingListCreated struct {
 	ShoppingList *ShoppingList
 }
 
-func (ShoppingListCreated) EventName() string { return "depot.ShoppingListCreated"}
+func (ShoppingListCreated) EventName() string { return "depot.ShoppingListCreated" }
 
-type ShoppingListCanceled struct{
+func (ShoppingListCreated) Key() string { return ShoppingListCreatedEvent}
+
+type ShoppingListCanceled struct {
 	ShoppingList *ShoppingList
 }
 
-func (ShoppingListCanceled) EventName() string { return "depot.ShoppingListCanceled"}
+func (ShoppingListCanceled) EventName() string { return "depot.ShoppingListCanceled" }
 
-type ShoppingListAssigned struct{
+func (ShoppingListCanceled) Key() string { return ShoppingListCanceledEvent}
+
+type ShoppingListAssigned struct {
 	ShoppingList *ShoppingList
-	BotID string
+	BotID        string
 }
 
-func (ShoppingListAssigned) EventName() string { return "depot.ShoppingListAssigned"}
+func (ShoppingListAssigned) EventName() string { return "depot.ShoppingListAssigned" }
 
-type ShoppingListCompleted struct{
+func (ShoppingListAssigned) Key() string { return ShoppingListAssignedEvent }
+
+type ShoppingListCompleted struct {
 	ShoppingList *ShoppingList
 }
 
 func (ShoppingListCompleted) EventName() string { return "depot.ShoppingListCompleted" }
+
+func (ShoppingListCompleted) Key() string { return ShoppingListCompletedEvent }
