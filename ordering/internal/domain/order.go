4fcd38bde57788eab4ad2667b6c86fb852dc44c1
irononet <andrewwanet9@gmail.com>
2024-04-01 00:49:41 +0100

    refactored the ordering/domain package

--- ordering/internal/domain/order.go
+++ ordering/internal/domain/order.go
@@ -4,9 +4,13 @@ import (
 	"github.com/stackus/errors"
 
 	"github.com/irononet/mallbots/internal/ddd"
+	"github.com/irononet/mallbots/internal/es"
 )
 
+const OrderAggregate = "ordering.Order"
+
 var (
+	ErrOrderAlreadyCreated = errors.Wrap(errors.ErrBadRequest, "the order cannot be created")
 	ErrOrderHasNoItems = errors.Wrap(errors.ErrBadRequest, "the order has no items")
 	ErrOrderCannotBeCancelled = errors.Wrap(errors.ErrBadRequest, "the order cannot be cancelled")
 	ErrCustomerIDCannotBeBlank = errors.Wrap(errors.ErrBadRequest, "the customer id cannot be blank")
@@ -17,44 +21,54 @@ var (
 	ErrOrderAlreadyCompleted = errors.Wrap(errors.ErrBadRequest, "the order is already completed")
 )
 
+var _ interface{
+	es.EventApplier
+	es.Snapshotter
+} = (*Order)(nil)
+
+func NewOrder(id string) *Order{
+	return &Order{
+		Aggregate: es.NewAggregate(id, OrderAggregate),
+	}
+}
+
 type Order struct{
-	ddd.AggregateBase
+	es.Aggregate
 	CustomerID string
 	PaymentID string
 	InvoiceID string
 	ShoppingID string
-	Items []*Item
+	Items []Item
 	Status OrderStatus
 }
 
-func CreateOrder(id, customerID, paymentID string, items []*Item) (*Order, error){
+func (Order) Key() string { return OrderAggregate }
+
+func(o *Order) CreateOrder(id, customerID, paymentID, shoppingID string, items []Item) error{
+	if o.Status != OrderUnknown{
+		return ErrOrderAlreadyCreated
+	}
+
 	if len(items) == 0{
-		return nil, ErrOrderHasNoItems
+		return ErrOrderHasNoItems
 	}
 
 	if customerID == ""{
-		return nil, ErrCustomerIDCannotBeBlank
+		return ErrCustomerIDCannotBeBlank
 	}
 
 	if paymentID == ""{
-		return nil, ErrPaymentIDCannotBeBlank
+		return ErrPaymentIDCannotBeBlank
 	}
 
-	order := &Order{
-		AggregateBase: ddd.AggregateBase{
-			ID: id,
-		},
+	o.AddEvent(OrderCreatedEvent, &OrderCreated{
 		CustomerID: customerID,
 		PaymentID: paymentID,
+		ShoppingID: shoppingID,
 		Items: items,
-		Status: OrderIsPending,
-	}
-
-	order.AddEvent(&OrderCreated{
-		Order: order,
 	})
-
-	return order, nil
+	
+	return nil
 }
 
 func (o *Order) Cancel() error{
@@ -64,9 +78,10 @@ func (o *Order) Cancel() error{
 
 	o.Status = OrderIsCancelled
 
-	o.AddEvent(&OrderCanceled{
-		Order: o,
+	o.AddEvent(OrderCanceledEvent, &OrderCanceled{
+		CustomerID: o.CustomerID,
 	})
+
 	return nil
 }
 
@@ -82,8 +97,10 @@ func (o *Order) Ready() error{
 		o.Status = OrderIsReady
 	}
 
-	o.AddEvent(&OrderReadied{
-		Order: o,
+	o.AddEvent(OrderReadiedEvent, &OrderReadied{
+		CustomerID: o.CustomerID,
+		PaymentID: o.PaymentID,
+		Total: o.GetTotal(),
 	})
 
 	return nil
@@ -96,9 +113,10 @@ func (o *Order) Complete(invoiceID string) error{
 	o.InvoiceID = invoiceID
 	o.Status = OrderIsCompleted
 
-	o.AddEvent(&OrderCompleted{
-		Order: o,
+	o.AddEvent(OrderCompletedEvent, &OrderCompleted{
+		InvoiceID: invoiceID,
 	})
+
 
 	return nil
 }
@@ -111,4 +129,58 @@ func (o Order) GetTotal() float64{
 	}
 
 	return total
+}
+
+func (o *Order) ApplyEvent(event ddd.Event) error{
+	switch payload := event.Payload().(type){
+	case *OrderCreated:
+		o.CustomerID = payload.CustomerID
+		o.PaymentID = payload.PaymentID
+		o.ShoppingID = payload.ShoppingID
+		o.Items = payload.Items
+		o.Status = OrderIsPending
+
+	case *OrderCanceled:
+		o.Status = OrderIsCancelled
+
+	case *OrderReadied:
+		o.Status = OrderIsReady
+	
+	case *OrderCompleted:
+		o.InvoiceID = payload.InvoiceID
+		o.Status = OrderIsCompleted
+
+	default:
+		return errors.ErrInternal.Msgf("%T received the event %s with unexpected payload %T", o, event.EventName(), payload)
+	}
+
+	return nil
+}
+
+func (o *Order) ApplySnapshot(snapshot es.Snapshot) error{
+	switch ss := snapshot.(type){
+	case *OrderV1:
+		o.CustomerID = ss.CustomerID
+		o.PaymentID = ss.PaymentID
+		o.InvoiceID = ss.InvoiceID
+		o.ShoppingID = ss.ShoppingID
+		o.Items = ss.Items
+		o.Status = ss.Status
+
+	default:
+		return errors.ErrInternal.Msgf("%T received the unexpected snapshot %T", o, snapshot)
+	}
+
+	return nil
+}
+
+func (o *Order) ToSnapshot() es.Snapshot{
+	return &OrderV1{
+		CustomerID: o.CustomerID,
+		PaymentID: o.PaymentID,
+		InvoiceID: o.InvoiceID,
+		ShoppingID: o.ShoppingID,
+		Items: o.Items,
+		Status: o.Status,
+	}
 }
