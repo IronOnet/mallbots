6f984b1c4430dcceee72c9f5acbb97dd28e74478
irononet <andrewwanet9@gmail.com>
2024-04-01 01:10:24 +0100

    refactored product events in stores

--- stores/internal/domain/product_events.go
+++ stores/internal/domain/product_events.go
@@ -1,13 +1,39 @@
 package domain
 
+const (
+	ProductAddedEvent = "stores.ProductAdded"
+	ProductRebrandedEvent = "stores.ProductRebranded"
+	ProductPriceIncreasedEvent = "stores.ProductPriceIncreased"
+	ProductPriceDecreasedEvent = "stores.ProductPriceDecreased"
+	ProductRemovedEvent = "stores.ProductRemoved"
+)
+
 type ProductAdded struct {
-	Product *Product
+	StoreID string
+	Name string
+	Description string
+	SKU string
+	Price float64
 }
+
+func (ProductAdded) Key() string { return ProductAddedEvent }
 
 func (ProductAdded) EventName() string { return "stores.ProductAdded" }
 
+type ProductRebranded struct{
+	Name string
+	Description string
+}
+
+func (ProductRebranded) Key() string { return ProductRebrandedEvent }
+
+type ProductPriceChanged struct{
+	Delta float64
+}
+
 type ProductRemoved struct {
-	Product *Product
 }
 
 func (ProductRemoved) EventName() string { return "stores.ProductRemoved" }
+
+func (ProductRemoved) Key() string { return ProductRemovedEvent }
