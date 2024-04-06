c880b89320f3fbf22df2c8f28ddb4f213b45dfba
irononet <andrewwanet9@gmail.com>
2024-04-06 12:14:49 +0100

    refactored the domain module

--- stores/internal/domain/mall_repository.go
+++ stores/internal/domain/mall_repository.go
@@ -4,18 +4,18 @@ import (
 	"context"
 )
 
-type MallStore struct{
-	ID string
-	Name string
-	Location string
+type MallStore struct {
+	ID            string
+	Name          string
+	Location      string
 	Participating bool
 }
 
-type MallRepository interface{
+type MallRepository interface {
 	AddStore(ctx context.Context, storeID, name, location string) error
 	SetStoreParticipation(ctx context.Context, storeID string, participating bool) error
 	RenameStore(ctx context.Context, storeID, name string) error
 	Find(ctx context.Context, storeID string) (*MallStore, error)
-	All(ctx context.Context, storeID string) (*MallStore, error)
+	All(ctx context.Context) ([]*MallStore, error)
 	AllParticipating(ctx context.Context) ([]*MallStore, error)
 }
